eca766330cc07278b3ccfd6dfa6cd6468fedefcd
deannabosschert <deanna.bosschert@gmail.com>
2022-01-13 20:06:35 +0100

    feat: add variables

--- src/scripts/lib/filters/add-unit.js
+++ src/scripts/lib/filters/add-unit.js
@@ -2,8 +2,8 @@
 module.exports = async function addUnit(property) {
     if (property == 'font-size' || property == 'line-height') {
         return 'px'
-    } else if (property == 'width' || property == 'height') {
-        return 'rem'
+    } else if (property == 'width' || property == 'height' || property == 'mobile' || property == 'desktop') {
+        return 'px'
     } else {
         return ''
     }
--- src/scripts/lib/filters/generate-variable-names.js
+++ src/scripts/lib/filters/generate-variable-names.js
@@ -1,15 +1,16 @@
-const variables = require('../../../_data/styles/variable-naming.js')
+const variables = require('../../../_data/styles/variable-naming.json')
 const addUnit = require('./add-unit.js')
 
 module.exports = async function generateVariableNames(data) {
     let allVariables = ''
     for (let property in data) { // each property (e.g. 'width' or 'height') inside type (e.g. 'spacing')
         let variableNames = ''
+    
         let valueArray = data[property]
         let unit = await addUnit(property)
 
         for (let i = 0; i < valueArray.length; i++) {
-            variableNames += `\n $${property}-${variables[i]}: ${valueArray[i]}${unit};`
+            variableNames += `\n $${property}-${variables.names[i]}: ${valueArray[i]}${unit};`
         }
         allVariables += variableNames + '\n'
     }
