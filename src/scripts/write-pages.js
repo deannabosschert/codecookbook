dcf7421d4afae59a06a903b18418105759254251
deannabosschert <deanna.bosschert@gmail.com>
2022-01-17 11:40:20 +0100

    feat: disable write-pages

--- src/scripts/write-pages.js
+++ src/scripts/write-pages.js
@@ -1,63 +1,47 @@
 // icarus flew too close to the sun and now has to write spaghetti code to generate all these pages quickly
+// don't forget to remove page from this flow once written (although disabled altogether for now)
 const fs = require('fs')
 const getData = require('./lib/get-data.js')
-
 module.exports = function writePages() {
     lekkerLoopen(getData('navigation')) // fetch data from sidebar
 }
 
-
 function lekkerLoopen(data) {
     let sidebar = data.sidebar
-    for (let category in sidebar) { // introduction, design, development..
-        // let category = 'design'
+    for (let category in sidebar) { // 'design', parent
         let categoryData = sidebar[category]
         for (let page in categoryData) {
             let pageData = categoryData[page]
-            for (let subpage in pageData) {
-                //   let subpage = 'ontwerp' h1
-
+            for (let subpage in pageData) { // 'ontwerp'. h1
                 let pageContent = {
                     h1: subpage,
                     content: []
                 }
-
                 let subpageData = pageData[subpage]
-
                 subpageData.forEach(function (item) {
                     let contentContent = []
-                    for (let key in item) {
-                        // let key = 'variabelen'  h2
+                    for (let key in item) { // 'variabelen'  h2
                         let contentList = item[key]
-
                         contentContent.push({
                             h2: key,
                             content: contentList
                         })
-
                         pageContent.content.push(contentContent)
                     }
-
                 })
-
                 makeHTML(category, pageContent)
             }
-
-
         }
     }
 }
-
 
 function makeHTML(parent, data) {
     const lm = require('lorem-memesum')
     let sentenceWords = lm.sentence.randomSentence()
     let html = ''
     let pageName = data.h1
-
     data.content.forEach(function (item) {
         let section = ''
-
         let h2 = item[0].h2
         let articles = item[0].content
         section += `<section class="${parent}-${pageName}-subpage-${h2}">`
@@ -71,17 +55,19 @@ function makeHTML(parent, data) {
         section += `</section>`
         html += section
     })
-    writePages(parent, pageName, html)
+    writePages(parent, pageName, html, false)
 }
 
-function writePages(parent, page, html) {
+function writePages(parent, page, html, toGenerate) {
     if (page == 'ontwerp' || page == 'index') {
         return
+    } else if (toGenerate == false){
+        return
     } else {
-    let filepath = `src/pages/${parent}/${page}.html`
-    fs.writeFile(filepath, html, (err) => {
-        if (err) throw err
-        console.log(`The html-page for ${parent}/${page} has been generated from data and saved!`)
-    })
-}
+        let filepath = `src/pages/${parent}/${page}.html`
+        fs.writeFile(filepath, html, (err) => {
+            if (err) throw err
+            console.log(`The html-page for ${parent}/${page} has been generated from data and saved!`)
+        })
+    }
 }
