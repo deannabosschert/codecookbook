0d6975dd02ef66f8c4b479ea56d7c8ff256d8f10
deannabosschert <deanna.bosschert@gmail.com>
2022-01-19 21:36:33 +0100

    feat: add gantt graph categories

---
+++ assets/js/charts/lib/gantt-categories.js
@@ -0,0 +1,2 @@
+let categories = ["Meeting/presentatie","Klantcontact/meeting","Administratie","Intern overleg","?ux?","Design","Content","Onderzoek","Reviewen","Feedback verwerken","Security","?dev?","Development","Bugfix","Refactor/optimalisatie"]
+export { categories }
