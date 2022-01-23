9d91de009cfc5e569d79131c47ae0c647b31f974
deannabosschert <deanna.bosschert@gmail.com>
2022-01-23 01:35:54 +0100

    feat: add more content

--- assets/js/github-api.js
+++ assets/js/github-api.js
@@ -1,6 +1,5 @@
-export default async function githubAPI(containerID, repo, tag, state) {
+export default async function githubAPI(containerID, api, repo, tag, state) {
     const endpoint = "https://api.github.com"
-    const api = "/repos"
     const account = "/deannabosschert"
     const limit = "30"
     if (state == "") {
@@ -9,55 +8,94 @@ export default async function githubAPI(containerID, repo, tag, state) {
         state = `?&state=${state}`
     }
     const apiLink = `${endpoint}${api}${account}${repo}${tag}${state}`
-
+    console.log(apiLink)
     let ghData = await fetchData(apiLink)
-    let items = mapData(ghData)
+    let items = mapData(ghData, repo)
     let itemCards = makeHTML(items)
-
-    return renderData(containerID, itemCards)
+    if (items.health_percentage) {
+        renderObject(containerID, itemCards)
+    } else {
+        return renderData(containerID, itemCards)
+    }
 }
 
-function mapData(data) {
-    return data.map(item => {
-        if (item.user && item.body) {
-            return { //issues
-                title: item.title,
-                body: item.body,
-                number: item.number,
-                state: item.state,
-                created_at: item.created_at,
-                url: item.html_url,
-                user: item.user.login
-            }
-        } else if (item.color && item.name) { //labels
-            return {
-                name: item.name,
-                description: item.description,
-                color: item.color,
-                id: item.id,
-                url: item.url
-            }
-        } else {
-            return { //milestones
-                title: item.title,
-                description: item.description,
-                number: item.number,
-                state: item.state,
-                closed_issues: item.closed_issues,
-                open_issues: item.open_issues,
-                created_at: item.created_at,
-                due: item.due_on,
-                url: item.html_url
-            }
+function mapData(data, repo) {
+    if (data.health_percentage) {
+        return { // metrics
+            title: repo,
+            description: data.description,
+            health_percentage: data.health_percentage,
+            documentation: data.documentation,
+            license: data.files.license,
+            readme: data.files.readme.html_url
         }
-    })
+    } else {
+        return data.map(item => {
+            if (item.user && item.body) {
+                return { //issues
+                    title: item.title,
+                    body: item.body,
+                    number: item.number,
+                    state: item.state,
+                    created_at: item.created_at,
+                    url: item.html_url,
+                    user: item.user.login
+                }
+            } else if (item.color && item.name) { //labels
+                return {
+                    name: item.name,
+                    description: item.description,
+                    color: item.color,
+                    id: item.id,
+                    url: item.url
+                }
+            } else {
+                return { //milestones
+                    title: item.title,
+                    description: item.description,
+                    number: item.number,
+                    state: item.state,
+                    closed_issues: item.closed_issues,
+                    open_issues: item.open_issues,
+                    created_at: item.created_at,
+                    due: item.due_on,
+                    url: item.html_url
+                }
+            }
+        })
+    }
+    // else {
+    //     console.log('onbekend type')
+    // }
 }
 
 function makeHTML(data) {
-    return data.map(item => {
-        let url = item.url
-        if (item.user && item.body) { // issues
-            let card = `
+    if (data.health_percentage) {
+        console.log(data)
+        let card = `
+                <article class="github-embed-card">
+                    <div class="github-embed-card-header">
+                        <h4>${data.title}</h4>
+                        <p>${data.health_percentage}% complete</p>
+                    </div>
+                    <div class="card-body">
+                        <p>${data.description}</p>
+            `
+        if (data.license) {
+            card += `<p>${data.license.name}</p>`
+        }
+        card += `
+                        <a href="${data.documentation}" target="_blank">link naar documentation</a>
+                        <a href="${data.readme}" target="_blank">link naar readme</a>
+                    </div>
+                </article>
+        `
+        return card
+    } else {
+        return data.map(item => {
+            let url = item.url
+            if (item.user && item.body) { // issues
+                let card = `
                 <article class="github-embed-card">
                     <div class="github-embed-card-header">
                         <h4>${item.title}</h4>
@@ -70,10 +108,9 @@ function makeHTML(data) {
                     </div>
                 </article>
                 `
-
-            return card
-        } else if (item.color && item.name) { // labels
-            let card = `
+                return card
+            } else if (item.color && item.name) { // labels
+                let card = `
                     <article class="github-embed-card" style="border:5px solid #${item.color}">
                         <div class="github-embed-card-header">
                             <h4>${item.name}</h4>
@@ -86,9 +123,9 @@ function makeHTML(data) {
                         </div>
                     </article>
                     `
-            return card
-        } else if (url.includes('milestone')) { // milestones
-            let card = `
+                return card
+            } else if (url.includes('milestone')) { // milestones
+                let card = `
                     <article class="github-embed-card">
                         <div class="github-embed-card-header">
                             <h4>${item.title}</h4>
@@ -103,31 +140,28 @@ function makeHTML(data) {
                         </div>
                     </article>
               `
-            return card
-        } else {
-            let card = `
+                return card
+            } else {
+                let card = `
                     <article class="github-embed-card">
                         <div>class="github-embed-card-header">
                             <h4>${item.title}</h4>
     `
-            if (item.created_at) {
-                card += `<p>created: ${item.created_at}</p>`
+                if (item.created_at) {
+                    card += `<p>created: ${item.created_at}</p>`
+                }
+                card += `</div><div class="card-body">`
+                if (item.body) {
+                    card += `<p>${item.body}</p>`
+                }
+                if (item.url) {
+                    card += `<a href="${item.url}" target="_blank" >link naar milestone #${item.number}</a>`
+                }
+                card += `</div></article>`
+                return card
             }
-            card += `</div><div class="card-body">`
-            if (item.body) {
-                card += `<p>${item.body}</p>`
-
-            }
-            if (item.url) {
-                card += `<a href="${item.url}" target="_blank" >link naar milestone #${item.number}</a>`
-            }
-
-            card += `</div></article>`
-            return card
-
-        }
-
-    })
+        })
+    }
 }
 
 function renderData(containerID, data) {
@@ -146,7 +180,6 @@ function renderData(containerID, data) {
                         max-width: 100%;
                         box-shadow: 0 1px 3px 0 rgba(0, 0, 0, .1), 0 1px 2px 0 rgba(0, 0, 0, .06);
                     }
-
                     .github-embed-card-header {
                         display: flex;
                         flex-direction: row;
@@ -155,17 +188,20 @@ function renderData(containerID, data) {
                         flex-wrap: wrap;
                         margin-bottom: 0.5rem;
                     }
-
                     .github-embed-card h4 {
                         color: #67c8f2ff;
                     }
-
                     .github-embed-card p {
                         max-width: fit-content
                     }
                 </style>
                       `
     githubCard.innerHTML += cardstyling
+}
+
+function renderObject(containerID, data) {
+    let githubCard = selectElement(`#${containerID}`)
+    githubCard.innerHTML = data
 }
 
 function selectElement(selector) {
